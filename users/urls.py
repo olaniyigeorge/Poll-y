b8925df4a1ec257cfa2251b52d2c51a6fbf057b9
olaniyigeorge <olaniyigeorge77@gmail.com>
2023-05-06 16:04:24 +0100

    Added connect url for follow and unfollow functionalities

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('signup/', views.signup, name='signup'),
-    path("<str:username>", views.profile, name='profile')
+    path("<str:username>", views.profile, name='profile'),
+    path("connect/<int:user_id>", views.connect, name='connect')
     
     
 ] 
